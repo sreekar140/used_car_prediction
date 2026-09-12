33c7e42836739b792ab66dd4712f1b77eea60ad6
Sreekar Chowdary <sreekarchowdary14@gmail.com>
2026-09-12 16:57:59 +0530

    Allow Vercel frontend in CORS

--- backend/settings.py
+++ backend/settings.py
@@ -135,7 +135,6 @@ MAILERS = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://used-car-prediction-git-main-scam5.vercel.app"
+    "https://used-car-prediction-nine.vercel.app",
 ]
 
